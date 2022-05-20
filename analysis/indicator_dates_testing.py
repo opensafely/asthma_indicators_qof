c7136d26d21ded301420368445f70b14a70f84b3
rose-higgins <95700645+rose-higgins@users.noreply.github.com>
2022-05-20 11:25:49 +0100

    Further updates to code

--- analysis/indicator_dates_testing.py
+++ analysis/indicator_dates_testing.py
@@ -1,20 +1,20 @@
 
-# Import functions
-import pandas as pd
-from pathlib import Path
-import os
+# # Import functions
+# import pandas as pd
+# from pathlib import Path
+# import os
 
-BASE_DIR = Path(__file__).parents[1]
-INPUT_DIR = BASE_DIR / "output/joined"
-OUTPUT_DIR = BASE_DIR / "output/test"
+# BASE_DIR = Path(__file__).parents[1]
+# INPUT_DIR = BASE_DIR / "output/joined"
+# OUTPUT_DIR = BASE_DIR / "output/test"
 
-print('this has run')
+# print('this has run')
 
-df = pd.read_csv(os.path.join(INPUT_DIR, f'input_ast007_2019-03-01.csv'))
+# df = pd.read_csv(os.path.join(INPUT_DIR, f'input_ast007_2019-03-01.csv'))
 
 
 
-df = pd.DataFrame(list())
-df.to_csv(os.path.join(OUTPUT_DIR, f'test.csv'))
+# df = pd.DataFrame(list())
+# df.to_csv(os.path.join(OUTPUT_DIR, f'test.csv'))
 
 

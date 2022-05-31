35ce6113623c1435ab5c4ac14d16812153430e2e
rose-higgins <95700645+rose-higgins@users.noreply.github.com>
2022-05-31 11:26:10 +0100

    testing change to numbers

--- analysis/config.py
+++ analysis/config.py
@@ -1,8 +1,8 @@
 #study start date.  should match date in project.yaml
-start_date = "2019-03-01"
+start_date = "2019-01-01"
 
 #study end date.  should match date in project.yaml
-end_date = "2022-03-31"
+end_date = "2020-03-31"
 
 #demographic variables by which code use is broken down
 #select from ["sex", "age_band", "region", "imd", "ethnicity", "learning_disability"]
